3c5314f8206548fb15f4e46e3f317dcf17e2028d
AGRohl <al036391@S04-PC02>
2016-12-14 13:59:19 +0100

    neptunov2

--- neptuno/session.php
+++ neptuno/session.php
@@ -1,8 +1,8 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+session_start(); //inicia la sesión
+if (!isset($_SESSION['count'])) { //comprueba si la variable de sesion count existe
+  $_SESSION['count'] = 0; //crea una variable de sesión llamada count
+} else {
+  $_SESSION['count']++; //toma el valor de la variable de sesión y lo incrementa.
+}
